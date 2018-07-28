048b1b2e76bd00beaa3b3d6b2c7f4f00b724e60c
Luis Perez <luis0@stanford.edu>
2018-07-27 20:08:35 -0700

    Finished new questions and partial ranking

--- Serena/Controller/QuestionController.swift
+++ Serena/Controller/QuestionController.swift
@@ -15,6 +15,9 @@ class QuestionController: UIViewController {
     @IBOutlet weak var option3: UIButton!
     @IBOutlet weak var option4: UIButton!
     
+    // This is the tag of the "correct" button.
+    var correctTag: Int?
+    
     // I will return only the four elements that were chosen.
     func getChoices(originals: [NewsItem]) -> (NewsItem, NewsItem, NewsItem, NewsItem) {
         // let's do this randomly
@@ -25,19 +28,46 @@ class QuestionController: UIViewController {
         return (originals[0], originals[1], originals[2], originals[3])
     }
     
+    @IBAction func sendCorrect(_ sender: Any) {
+        //first button pressed
+        let button = sender as! UIButton
+        if let correct = correctTag {
+            if button.tag == correct {
+                let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+                let nextViewController = storyBoard.instantiateViewController(withIdentifier: "CorrectViewController")
+                self.navigationController?.pushViewController(nextViewController, animated: true)
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationController?.title = "News Question"
         
         NewsItem.fetchTrendingNews(callback: {(news: [NewsItem], errorMsg: String?) in
             let (item1, item2, item3, item4) = self.getChoices(originals: news)
             
+            var maxItem: NewsItem = item1
+            var correctTag = 0
+            for (tag, item) in [item1, item2, item3, item4].enumerated() {
+                if (item.rankint > maxItem.rankint) {
+                    maxItem = item
+                    correctTag = tag
+                }
+            }
+            
+            // We now know the max item.
+            
             DispatchQueue.main.async {
-                self.option1.titleLabel?.text = item1.title
-                self.option2.titleLabel?.text = item2.title
-                self.option3.titleLabel?.text = item3.title
-                self.option4.titleLabel?.text = item4.title
+                self.option1.setTitle(item1.title, for: UIControlState.normal)
+                self.option2.setTitle(item2.title, for: UIControlState.normal)
+                self.option3.setTitle(item3.title, for: UIControlState.normal)
+                self.option4.setTitle(item4.title, for: UIControlState.normal)
             }
         })
+        
 
         // Do any additional setup after loading the view.
     }
@@ -46,6 +76,7 @@ class QuestionController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
     /*
